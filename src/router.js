64673f520d898805b8ee8ba47beffce025045fc5
Kevin Oustry <oustrykevin@gmail.com>
2020-02-24 00:14:18 +0100

    feat(scraping): setup scraping controller

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const companyController = require('./controller/companyController')
+const scrapingController = require('./controller/scrapingController')
 
 
 router.use(express.json())
 router.use('/companies', companyController)
+router.use('/scraping', scrapingController)
 
 module.exports = router
