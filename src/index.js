49d76a183736071e2f4dde0084b52027dc7ffa9f
Kevin Oustry <oustrykevin@gmail.com>
2020-02-25 12:30:46 +0100

    fix(scraping): throw if a batch fail

--- src/index.js
+++ src/index.js
@@ -29,24 +29,47 @@ async function initData() {
         }
     } else {
         try {
-            console.log("=> Loading data by scraping")
-            const jobs = await scrapingService.scrapDataByCity(SEARCH_URL, DATA_URL, CITY)
-            const workerPool = 20
+            // var p1 = Promise.resolve(3);
+            // var p2 = 1337;
+            // var p3 = new Promise((resolve, reject) => {
+            //     setTimeout(resolve, 5000, 'foo');
+            // })
+            // console.log([p1, p2, p3]);
+            
+            // Promise.all([p1, p2, p3]).then(values => { 
+            //     console.log(values); // [3, 1337, "foo"] 
+            // })
 
-            for (let i = 0 ; i < jobs.length ; i += workerPool) {
-                let jobsToRun = []
-                const workers = jobs.slice(i, workerPool + i)
-                for (const job of workers) {
-                    jobsToRun.push(job())
+            console.log("=> Loading data by scraping")
+            // console.log("AAA")
+            const jobs = await scrapingService.scrapDataByCity(SEARCH_URL, DATA_URL, CITY)
+            // console.log("BBB")
+            // console.log(jobs)
+            // console.log("CCC")
+            const size = 30
+            for (let i = 0; i < jobs.length; i+=size) {
+                
+                const jobsToRun = jobs.slice(i, size+i)
+                let prom = []
+                console.log("=> Start running " + (size+i) + " workers of " + jobs.length)
+                for (const job of jobsToRun) {
+                    prom.push(job())
                 }
                 try {
-                    await Promise.all(jobsToRun)
+                    await Promise.all(prom)
+                    console.log("=> Finish running " + (size+i) + " workers of " + jobs.length)
                 } catch (error) {
-                    console.error(error)
+                    throw error
                 }
-                await utils.sleep(10000)
-                jobsToRun = []
+                await utils.sleep(5000)
+                if (i % 100 === 0) await utils.sleep(15000)
+                prom = []
             }
+
+            // NEED TO SPLIT JOBS IN ARRAY OF ≈ 10 TASKS 
+            // await Promise.all(jobs)
+            // console.log("DDD")
+            // utils.sleep(2000)
             // console.log("Saving data in file")
             // fs.writeFileSync(PATH, JSON.stringify(companies))
         } catch (error) {
